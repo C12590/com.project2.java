9c8811a013daa975d1913335c0c8c8299e825f43
‘caochun’ <caochun@zgshbfw.com>
2023-04-21 21:35:41 +0800

    部分新增加

---
+++ src/com/zgshbfw/test/Account.java
@@ -0,0 +1,44 @@
+package com.zgshbfw.test;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate;
+
+    public Account ( int id, double balance, double annualInterestRate )
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getAnnualInterestRate() {
+        return  annualInterestRate;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public void withdraw(double amount) { //取钱
+        if(balance < amount) {
+            System.out.println(余额不足，请充值);
+            return;
+        }
+        balance -= amount;
+            System.out.print("取錢成功，" + amount);
+    }
